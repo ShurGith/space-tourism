6574af9d485d1ee0d0cbc2ca7b8e60ced102510d
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-05-23 01:18:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
