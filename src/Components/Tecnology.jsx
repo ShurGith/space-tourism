bee90a72cf8f5359072a5dc289a9a09cd3ef6a23
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-05-24 18:26:58 +0200

    technology

--- src/Components/Tecnology.jsx
+++ src/Components/Tecnology.jsx
@@ -10,12 +10,12 @@ import victor from '../assets/technology/image-victor-glover.webp'
 import anosheh from '../assets/technology/image-anousheh-ansari.png'*/
 import data from '../assets/data.json'
 
-function ElementoBoton({ id, name, esActivado, onAction }) {
-  const claseCSS = esActivado ? 'size-4 rounded-full bg-white' : 'cursor-pointer size-4 rounded-full bg-white/50';
+function ElementoBoton({ id, esActivado, onAction }) {
+  const claseCSS = esActivado ? ' bg-white text-black' : 'text-white bg-white/0';
   return (
-    <div className={`${claseCSS} `} onClick={() => onAction(id)}>
-      {name}
-    </div>
+    <buttom className={`${claseCSS} cursor-pointer font-space border-1 border-white/30 size-16 flex justify-center items-center text-3xl rounded-full  text-black `} onClick={() => onAction(id)}>
+      {id + 1}
+    </buttom>
   )
 }
 
@@ -26,21 +26,20 @@ function Tecnology() {
   const [index, setIndex] = useState(0)
   const backgroundImage = isMobile ? '/assets/technology-mobile.jpg' : isTablet ? '/assets/technology-tablet.jpg' : '/assets/technology/background-technology-desktop.jpg';
   //const imagenes = [douglas, mark, victor, anosheh]
-  const crews = data.crew
-  const [imagen, setImagen] = useState('')
+  const technology = data.technology
+  //const [imagen, setImagen] = useState('')
 
   const [enlaces, setEnlaces] = useState([
     { id: 0, activado: true },
     { id: 1, activado: false },
     { id: 2, activado: false },
-    { id: 3, activado: false }
   ])
 
   const handleEnlace = (id) => {
     setEnlaces(enlaces.map(enlace =>
       enlace.id === id ? { ...enlace, activado: true } : { ...enlace, activado: false }
     ))
-    setImagen(imagenes[id])
+    // setImagen(imagenes[id])
     setIndex(id)
   }
 
@@ -50,18 +49,18 @@ function Tecnology() {
       <div className='flex flex-col-reverse  items-center justify-center lg:grid lg:grid-cols-5 gap-12 lg:place-items-center ' >
         <div className='flex flex-col px-4 lg:pl-30 col-span-3 items-start max-w-fit gap-9'>
           <h2 className='text-white lg:pb-24 font-extralight text-center lg:mb-6 text-xl tracking-[2.7px]'>
-            <span className='font-bold text-white/30 lg:mr-6'>02</span>
-            MEET YOUR CREW
+            <span className='font-space font-bold text-white/30 lg:mr-6'>03</span>
+            SPACE LAUNCH 101
           </h2>
           <div className='flex flex-col lg:gap-6 '>
-            <span className='font-space text-white/60 text-2xl uppercase'>{crews[index].role}</span>
-            <h2 className='font-space lg:pl-0 mt-2 lg:mt-0 text-white uppercase text-3xl lg:text-6xl'>{crews[index].name}</h2>
+            <span className='font-space text-white/60 text-xl uppercase'>THE TERMINOLOGY..</span>
+            <h2 className='font-space lg:pl-0 mt-2 lg:mt-0 text-white uppercase text-3xl lg:text-6xl'>{technology[index].name}</h2>
 
             <p className=' lg:w-3/5 px-4 tracking-[2px] text-white  font-extralight text-sm/6.5'>
-              {crews[index].bio}
+              {technology[index].description}
             </p>
           </div>
-          <div className='flex gap-10 justify-center lg:justify-start w-full'>
+          <div className='flex flex-col border gap-4 max-w-fit justify-center lg:justify-start w-full'>
             {enlaces.map(item => (
               <ElementoBoton
                 key={item.id}
@@ -73,7 +72,7 @@ function Tecnology() {
           </div>
         </div>
         <div className='flex flex-col col-span-2 gap-20 '>
-          <img src={imagen} alt="" className='lg:w-2xl object-contain lg:absolute lg:bottom-0 lg:right-0' />
+          <img src={technology[index].images.portrait} alt="" className='lg:w-2xl object-contain lg:absolute lg:bottom-0 lg:right-0' />
         </div>
       </div>
     </div>
